a58046fa1cadd27d93aa074f7be560ae6293dda3
Niels Robin-Aubertin <git@niels.fr>
2021-08-08 17:53:50 +0200

    Add random mode

--- apod/action.php
+++ apod/action.php
@@ -2,7 +2,21 @@
 
 $memory = $this->botService->load_memory($bot_id);
 $apod_api_key = $memory["apod_api_key"];
-$apod_data = json_decode(file_get_contents("https://api.nasa.gov/planetary/apod?api_key=$apod_api_key"), true);
+
+if (time() - intval($memory["last_post_at"]) < intval($memory["post_interval"])) {
+  return;
+}
+
+if (!empty($memory["random"]) && $memory["random"]) {
+  # choose a random date
+  $format = "Y-m-d";
+  $start = (DateTime::createFromFormat("Y-m-d", "2015-01-01"))->getTimestamp();
+  $end = time();
+  $date = date("Y-m-d", rand($start, $end));
+  $apod_data = json_decode(file_get_contents("https://api.nasa.gov/planetary/apod?api_key=$apod_api_key&date=$date"), true);
+} else {
+  $apod_data = json_decode(file_get_contents("https://api.nasa.gov/planetary/apod?api_key=$apod_api_key"), true);
+}
 
 $url = $apod_data['url'];
 if (array_key_exists("hdurl", $apod_data)) {
@@ -32,7 +46,7 @@ if (
 $message = [
   "data" => [
     "text" => $text,
-    "title" => $title,
+    "title" => $title . " [$date]",
   ],
   "parent_id" => $memory["parent_id"] ?? "",
 ];
@@ -43,5 +57,7 @@ array_push($memory["last_posted"], $url);
 if (count($memory["last_posted"]) > 10) {
   array_shift($memory["last_posted"]);
 }
+
+$memory["last_post_at"] = time();
 
 $this->botService->dump_memory($bot_id, $memory);
